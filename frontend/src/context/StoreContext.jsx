6bc2c338d59be6f22a3ad93d4b924c4e94125342
thejasree25 <g.thejasree15@gmail.com>
2025-07-11 18:03:12 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
-  const url = "http://localhost:5000";
+  const url = "https://foodedelbackend.onrender.com";
 
   const addToCart = async (itemId) => {
     setCartItems((prev) => ({
