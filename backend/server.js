cd3c2482a5296d282def5c90fcebc7cbf7760984
thejasree25 <g.thejasree15@gmail.com>
2025-07-11 18:31:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://foodedelfrontend.onrender.com", // your frontend URL
+  credentials: true,
+}));
 app.use(express.json());
 
 // Connect MongoDB
